9eeec15fbcd5c06a6fd907619cea83863c33ae87
apollo <jackratermann@gmail.com>
2025-10-24 21:16:09 -0700

    feat: Cool shit

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,23 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "Mr-LLLLL/interestingwords.nvim" },
   config = function()
     require("lualine").setup({
       options = {
         theme = "rose-pine", -- or "tokyonight", "dracula", "catppuccin", "auto"
       },
+      sections = {
+        -- your other sections...
+        lualine_x = {
+          {
+            require("interestingwords").lualine_get,
+            cond = require("interestingwords").lualine_has,
+            color = { fg = "#ff9e64" },
+          },
+        },
+      },
     })
   end,
 }
+
 
